7d3b0c74fde0e1566887bbda84249a07c400fb40
tomogwen <tdavies@turing.ac.uk>
2023-12-01 16:14:51 +0000

    rust compiler + chatgpt = 2*

---
+++ day-01/rust_tomogwen/target/debug/deps/rust_tomogwen-a8b701adff5b2477.d
@@ -0,0 +1,5 @@
+/Users/tdavies/Documents/code/advent-of-code-2023/day-01/rust_tomogwen/target/debug/deps/rust_tomogwen-a8b701adff5b2477: src/main.rs
+
+/Users/tdavies/Documents/code/advent-of-code-2023/day-01/rust_tomogwen/target/debug/deps/rust_tomogwen-a8b701adff5b2477.d: src/main.rs
+
+src/main.rs:
