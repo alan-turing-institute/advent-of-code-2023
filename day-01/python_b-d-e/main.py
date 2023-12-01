ffcbf1d88ec813a120713a214a4ecbd782f06e60
Benjamin Etheridge <betheridge@turing.ac.uk>
2023-12-01 18:37:07 +0000

    add typing

--- day-01/python_b-d-e/main.py
+++ day-01/python_b-d-e/main.py
@@ -1,40 +1,43 @@
+
 # advent of code - day 1
 import re
+from typing import List, Dict
+
 
 # read input
 with open('input.txt', 'r') as f:
-    input_string = f.readlines()
+    input_string: List[str] = f.readlines()
 
 
-def part_one(input_string):
+def part_one(input_string: List[str]) -> None:
     # ugly enumeration
-    total = 0
+    total: int = 0
 
-    nums = [str(x) for x in[0,1,2,3,4,5,6,7,8,9]]
+    nums: List[str] = [str(x) for x in[0,1,2,3,4,5,6,7,8,9]]
 
     for string in input_string:
-        start = 0
+        start: int = 0
         while string[start] not in nums:
             start += 1
-        end = len(string) - 1
+        end: int = len(string) - 1
         while string[end] not in nums:
             end -= 1
-        concat = string[start] + string[end]
+        concat: str = string[start] + string[end]
         total += int(concat)
 
     print(total)
 
 
-def part_two(input_string):
+def part_two(input_string: List[str]) -> None:
     # nicer (but possibly more intensive) regex solution
     one_digit_number_regex = re.compile(r"(?=(one|two|three|four|five|six|seven|eight|nine|\d))")
-    num_ref = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
-    total = 0
+    num_ref: Dict[str, str] = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    total: int = 0
 
     for l in input_string:
-        matches = one_digit_number_regex.findall(l)
-        first = matches[0] if matches[0] not in num_ref else num_ref[matches[0]]
-        last = matches[-1] if matches[-1] not in num_ref else num_ref[matches[-1]]
+        matches: List[str] = one_digit_number_regex.findall(l)
+        first: str = matches[0] if matches[0] not in num_ref else num_ref[matches[0]]
+        last: str = matches[-1] if matches[-1] not in num_ref else num_ref[matches[-1]]
         total += int(first + last)
 
     print(total)
