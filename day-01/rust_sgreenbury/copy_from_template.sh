15b7868edb4f2bf775dbb643b1be2f7260fa0300
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-12-04 09:31:20 +0000

    Add script for copying from template

---
+++ day-01/rust_sgreenbury/copy_from_template.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Map positional args
+DAY=$1
+PATH_USERNAME=$2
+SOURCE_PATH=$3
+DEST_PATH=$4
+
+# Make destination
+DEST_PATH_WITH_SUBPATH=${DEST_PATH}/day-$DAY/$PATH_USERNAME
+
+# Print constructed destination path
+echo "Source path: ${SOURCE_PATH}"
+echo "Destination path: ${DEST_PATH_WITH_SUBPATH}"
+echo ""
+read -p "Press enter if ok..."
+
+# Make path
+mkdir -p  $DEST_PATH_WITH_SUBPATH
+
+# Sync minimal files for the day
+rsync -azvu \
+    --include data/inputs/$DAY.txt \
+    --include data/examples/$DAY.txt \
+    --include bin/$DAY.rs \
+    --include helpers.rs \
+    --include lib.rs \
+    --include day.rs \
+    --include template/* \
+    --include template/commands/* \
+    --include main.rs \
+    --exclude target/ \
+    --exclude *.rs \
+    --exclude *.txt \
+    --exclude ".*" \
+    --exclude "data/puzzles/" \
+    $SOURCE_PATH/src \
+    $SOURCE_PATH/data \
+    $SOURCE_PATH/Cargo.toml \
+    $DEST_PATH_WITH_SUBPATH/./
+
+# Make README
+printf "[Solution](src/bin/$DAY.rs) for day $DAY.\n\nRun examples with:\n\`\`\`\ncargo test --release\n\`\`\`\n\nAnd solve inputs with:\n\`\`\`\ncargo run --bin ${DAY} --release\n\`\`\`\n" \
+    > $DEST_PATH_WITH_SUBPATH/README.md
