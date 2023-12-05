04b377e0631e0b4e60d36b1e969f6a8433c4382c
rchan26 <rchan@turing.ac.uk>
2023-12-05 11:33:48 +0000

    ☠️ format

--- day-05/julia_rchan26/day_five.jl
+++ day-05/julia_rchan26/day_five.jl
@@ -4,25 +4,27 @@ using Test
 test_input = readlines("test_input.txt")
 input = readlines("input.txt")
 
-function parse_input(input::Vector{String}, part_two::Bool=false)::Dict{String, Any}
+function parse_input(input::Vector{String}, part_two::Bool = false)::Dict{String,Any}
     # parse the input to get the seeds (as ranges if part_two), the maps and the order of the maps
     # maps are saved as a dictionary where the keys are the source ranges and 
     # the values are the differences you must add to get the destination
-    maps = Dict{String, Dict{UnitRange{Int64}, Int64}}()
+    maps = Dict{String,Dict{UnitRange{Int64},Int64}}()
     # initialize to empty
     seeds = Vector{Int64}()
     map_name = ""
-    maps_in_order = Vector{Union{String, SubString}}()
+    maps_in_order = Vector{Union{String,SubString}}()
 
     for line in input
         if occursin("seeds: ", line)
             seeds = parse.(Int64, split(split(input[1], ": ")[2], " "))
-            seeds = part_two ? [seeds[i]:seeds[i]+seeds[i+1] for i in 1:2:length(seeds)-1] : seeds
+            seeds =
+                part_two ? [seeds[i]:seeds[i]+seeds[i+1] for i = 1:2:length(seeds)-1] :
+                seeds
         elseif occursin("map:", line)
             # line of input giving a new map name
             map_name = rstrip(line, [' ', 'm', 'a', 'p', ':'])
             # initialise a new map
-            maps[map_name] = Dict{Int64, Int64}()
+            maps[map_name] = Dict{Int64,Int64}()
             # add the map name to the list of maps in order
             push!(maps_in_order, map_name)
         elseif line == ""
@@ -32,20 +34,22 @@ function parse_input(input::Vector{String}, part_two::Bool=false)::Dict{String, 
             # line of input giving the numbers and range
             destination_start, source_start, range_length = parse.(Int64, split(line, " "))
             source_range = source_start:(source_start+range_length-1)
-            merge!(maps[map_name], Dict(source_range => destination_start-source_start))
+            merge!(maps[map_name], Dict(source_range => destination_start - source_start))
         end
     end
 
     return Dict("seeds" => seeds, "maps" => maps, "maps_in_order" => maps_in_order)
 end
 
-function ☠️(input::Vector{String}, part_two::Bool=false)::Int64
+function ☠️(input::Vector{String}, part_two::Bool = false)::Int64
     # parse the input to get the seeds and maps
     parsed_input = parse_input(input, part_two)
     current_min = Inf64
 
     # if part_two, collect and reduce the seed ranges
-    parsed_input["seeds"] = part_two ? reduce(vcat, [collect(x) for x in parsed_input["seeds"]]) : parsed_input["seeds"]
+    parsed_input["seeds"] =
+        part_two ? reduce(vcat, [collect(x) for x in parsed_input["seeds"]]) :
+        parsed_input["seeds"]
 
     # for each seed, convert it using the maps and keep track of the minimum
     for seed in ProgressBar(parsed_input["seeds"])
@@ -79,7 +83,7 @@ end
 function part_two(input::Vector{String})::Int64
     # start from a final location and go backwards in the processes
     # keep increasing the final location until we find a valid starting seed
-    
+
     # parse the input to get the seeds and maps
     parsed_input = parse_input(input, true)
     final_location = 0
@@ -88,13 +92,13 @@ function part_two(input::Vector{String})::Int64
         converted_number = final_location
         for map_name in reverse(parsed_input["maps_in_order"])
             for (source_range, difference) in parsed_input["maps"][map_name]
-                if converted_number-difference in source_range
+                if converted_number - difference in source_range
                     converted_number -= difference
                     break
                 end
             end
         end
-        
+
         # if it's a valid seed, break and return
         if is_valid_seed(converted_number, parsed_input["seeds"])
             return final_location
