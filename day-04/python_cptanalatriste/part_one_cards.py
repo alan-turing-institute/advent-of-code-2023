09540bb05088d12729aebc720f7bf39ebf57958f
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2023-12-05 12:17:52 +0000

    Day 04 - Part 1

---
+++ day-04/python_cptanalatriste/part_one_cards.py
@@ -0,0 +1,38 @@
+def calculate_points(winning_numbers: set[int], your_numbers: set[int]) -> int:
+    matching_numbers: set[int] = winning_numbers.intersection(your_numbers)
+    if len(matching_numbers) == 0:
+        return 0
+    return 2 ** (len(matching_numbers) - 1)
+
+
+def string_to_set(line: str) -> set[int]:
+    return {
+        int(number_string)
+        for number_string in line.split(" ")
+        if number_string.isdigit()
+    }
+
+
+def parse_line(line: str) -> tuple[set[int], set[int]]:
+    _, all_numbers = line.split(":")
+    winning_numbers_string, your_numbers_string = all_numbers.split("|")
+    winning_numbers: set[int] = string_to_set(winning_numbers_string)
+    your_numbers: set[int] = string_to_set(your_numbers_string)
+
+    return winning_numbers, your_numbers
+
+
+def process_input_file(file_name: str) -> int:
+    answer: int = 0
+    with open(file_name) as input_file:
+        while line := input_file.readline():
+            winning_numbers, your_numbers = parse_line(line.rstrip("\n"))
+            card_points: int = calculate_points(winning_numbers, your_numbers)
+            answer += card_points
+            print(f"{card_points=} {line}")
+
+    return answer
+
+
+if __name__ == "__main__":
+    print(process_input_file("day-04/python_cptanalatriste/input.txt"))
