8d982c0bd324a0dad209f43cd22955089eab0d29
philswatton <philswatton@gmail.com>
2023-12-06 05:30:22 +0000

    day 6 done. why was day 5 so hard

---
+++ day-06/r_phil/day06.R
@@ -0,0 +1,17 @@
+file <- "input.txt"
+input <- readLines(file)
+
+# Process data
+input_strs <- regmatches(input, gregexpr("\\d+", input))
+dat <- as.data.frame(lapply(input_strs, as.numeric))
+names(dat) <- c("time", "best")
+
+# Part 1
+time_fn <- function(t) (t-(1:t)) * 1:t
+possible_times <- lapply(dat$time, time_fn)
+sapply(1:nrow(dat), function(x) sum(possible_times[[x]] > dat$best[x])) |> prod()
+
+# Part 2
+dat2 <- as.data.frame(lapply(input_strs, function(x) as.numeric(paste0(x, collapse=""))))
+names(dat2) <- c("time", "best")
+sum(time_fn(dat2$time) > dat2$best)
