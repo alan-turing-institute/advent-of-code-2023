ee589de8deeb8dc86988f5ede1921c72dd1050fd
James Geddes <jgeddes@turing.ac.uk>
2023-12-11 15:19:56 +0000

    Day 11 in Racket

--- day-11/racket_triangle-man/day11.rkt
+++ day-11/racket_triangle-man/day11.rkt
@@ -25,17 +25,35 @@ EOF
     (map string->list
          (with-input-from-file "input.txt" port->lines)))
 
-  (define *galaxies* (lists-indexes-of (expand-galaxy *image*) #\#))
-
+  (define *galaxies* (lists-indexes-of *image* #\#))
+  
   ;; Part 1
+
+  (define *row-offsets* (distances *image* 2))
+  (define *col-offsets* (distances (transpose *image*) 2))
+
   (for/sum ([gg (in-combinations *galaxies* 2)])
-    (match-let ([(list (cons x1 y1) (cons x2 y2)) gg])
-      (+ (abs (- x1 x2)) (abs (- y1 y2)))))
+    (match-let ([(list (cons r1 c1) (cons r2 c2)) gg])
+      (let ([x1 (list-ref *row-offsets* r1)]
+            [y1 (list-ref *col-offsets* c1)]
+            [x2 (list-ref *row-offsets* r2)]
+            [y2 (list-ref *col-offsets* c2)])
+        (+ (abs (- x1 x2)) (abs (- y1 y2))))))
 
   ;; Part 2
-  
-    )
 
+  (define *row-offsets2* (distances *image* 1000000))
+  (define *col-offsets2* (distances (transpose *image*) 1000000))
+
+  (for/sum ([gg (in-combinations *galaxies* 2)])
+    (match-let ([(list (cons r1 c1) (cons r2 c2)) gg])
+      (let ([x1 (list-ref *row-offsets2* r1)]
+            [y1 (list-ref *col-offsets2* c1)]
+            [x2 (list-ref *row-offsets2* r2)]
+            [y2 (list-ref *col-offsets2* c2)])
+        (+ (abs (- x1 x2)) (abs (- y1 y2))))))
+
+    )
 
 
 ;; ------------------------------------------------------------
@@ -46,23 +64,16 @@ EOF
 (define (blank-row? xs)
   (andmap blank? xs))
 
+(define (distances rows expansion-factor)
+  (for/fold ([offset 0]
+             [dists  '()]
+             #:result (reverse dists))
+            ([row (in-list rows)])
+    (values (+ offset (if (blank-row? row) expansion-factor 1))
+            (cons offset dists))))
+
 (define (transpose xss)
   (apply map list xss))
-
-(define (double-blank-rows xss)
-  (reverse
-   (let loop ([rest xss]
-              [acc  '()])
-     (if (null? rest)
-         acc
-         (let ([row (car rest)])
-           (if (blank-row? row)
-               (loop (cdr rest) (cons row (cons row acc)))
-               (loop (cdr rest) (cons row acc))))))))
-
-(define (expand-galaxy xss)
-  (double-blank-rows
-   (transpose (double-blank-rows (transpose xss)))))
 
 (define (lists-indexes-of xss x)
   (append*
