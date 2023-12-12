e29043f22c7ff598c7a6a6831962204394696fa8
James Geddes <jgeddes@turing.ac.uk>
2023-12-12 13:36:03 +0000

    Broken day 12 in Racket :(

--- day-12/racket_triangle-man/day12.rkt
+++ day-12/racket_triangle-man/day12.rkt
@@ -4,25 +4,55 @@
 
   (define *eg* (string->list "#.?.?"))
 
-  (define (make-tree xs)
+
+)
+
+(define (is-valid? xs groups skipping?)
+  (if (null? xs)
+      (null? groups)
+      (let ([x (car xs)]
+            [r (cdr xs)])
+        (or (and (char=? x #\.)
+                 skipping?
+                 (is-valid? r groups #t))
+            (and (char=? x #\#)
+                 (not skipping?)
+                 )
+            [()])))
+
+
+
+  ;; list? -> forest?
+  (define (make-forest xs)
     (if (null? xs)
-        null
+        '()
         (match (car xs)
-          [#\? (list (cons #\# (make-tree (cdr xs)))
-                     (cons #\. (make-tree (cdr xs))))]
-          [v   (list (cons v (make-tree (cdr xs))))])))
+          [#\? (list (cons #\# (make-forest (cdr xs)))
+                     (cons #\. (make-forest (cdr xs))))]
+          [v   (list (cons v (make-forest (cdr xs))))]))))
+
+ ;; tree : '(c . f) 
+(define (tree? t)
+  (and (pair? t)
+       (forest? (cdr t))))
+
+;; forest : '(t1 t2 ...)
+(define (forest? f)
+  (andmap tree? f))
+
+  
+(define/contract (flatten-forest f)
+  (-> forest? any/c)
+  (append*
+   (map flatten-tree f)))
+
+(define/contract (flatten-tree t)
+  (-> tree? any/c)
+  (match t
+    [(cons c '()) (list (list c))]
+    [(cons c f)
+     (map (λ (xs) (cons c xs))
+          (flatten-forest f))]))
+
 
  
-  ;; tree : '(rt1 rt2 ...)
-  (define (flatten-tree t)
-    (append*
-     (map flatten-rooted-tree t)))
-  
- ;; rooted-tree : '(c . (t1 t2 ...))
-  (define (flatten-rooted-tree rt)
-    (map (λ (t) (cons (car rt) t))
-         (map flatten-tree (cdr rt))))
-
-  
-  )
-
