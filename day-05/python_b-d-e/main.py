82dfcc483daa2d2805110be477c5809b6f736e3b
Benjamin Etheridge <betheridge@turing.ac.uk>
2023-12-12 23:06:04 +0000

    even my 'fast' version of 5b is still due to take 7 hours. am going to blame the GIL and leave it for now :(

--- day-05/python_b-d-e/main.py
+++ day-05/python_b-d-e/main.py
@@ -1,4 +1,8 @@
 from numpy import inf
+import tqdm
+
+import concurrent.futures
+
 
 def parse_input(filename):
     with open(filename, 'r') as file:
@@ -10,24 +14,88 @@ def parse_input(filename):
     }
     return parsed_data
 
-def part_one(seeds, mappings):
-
-    def get_next_value(num, mapping):
+def get_next_value(num, mapping):
         for rang in mapping:
             if int(rang[1]) <= num < int(rang[1])+int(rang[2]):
                 num = int(rang[0]) + num - int(rang[1])
                 return(num)
         return(num)
 
+
+def part_one(seeds, mappings):
+
     lowest_result = inf
 
+    seen_seeds = {}
+
+    # make tqdm
+    pg = tqdm.tqdm(total=len(seeds))
     for num in seeds:
-        for map in mappings:
-            num = get_next_value(num, mappings[map])
-        if num < lowest_result:
+        pg.update(1)
+        start = num
+        if start not in seen_seeds:
+            for map in mappings:
+                num = get_next_value(num, mappings[map])
+            seen_seeds[start] = num
+        if seen_seeds[start] < lowest_result:
             lowest_result = num
     return(lowest_result)
 
 
-data = parse_input("input.txt")
-print(part_one([int(s) for s in data['seeds'][0]],  data['maps']))
+#############################################
+
+
+def part_two_worker(num, mappings, seen_seeds):
+    start = num
+    if start not in seen_seeds:
+        for map in mappings:
+            num = get_next_value(num, mappings[map])
+        seen_seeds[start] = num
+    return num
+
+def part_two(seed_params, mappings):
+    seen_seeds = {}
+
+    # yield seed values one by one - avoid storing a huge list in memory
+    def generate_seeds(seed_params):
+        for i in range(0, len(seed_params), 2):
+            start = int(seed_params[i])
+            length = int(seed_params[i+1])
+            for j in range(length):
+                yield start + j
+
+    lowest_result = inf
+
+    num_seeds = sum([int(seed_params[i + 1]) for i in range(0, len(seed_params), 2)])
+    # make tqdm
+    pg = tqdm.tqdm(total=num_seeds)
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        # Use a list to store futures
+        futures = []
+
+        for num in generate_seeds(seed_params):
+            pg.update(1)
+
+            # Submit tasks to the executor
+            future = executor.submit(part_two_worker, num, mappings, seen_seeds)
+            futures.append(future)
+
+        # Wait for all tasks to complete
+        concurrent.futures.wait(futures)
+
+        # Get results from futures
+        for future in futures:
+            result = future.result()
+            if result < lowest_result:
+                lowest_result = result
+
+    return lowest_result
+
+
+
+if __name__ == '__main__':
+    # data = parse_input("example.txt")
+    data = parse_input("input.txt")
+    # print(part_one([int(s) for s in data['seeds'][0]],  data['maps']))
+    print(part_two([int(s) for s in data['seeds'][0]],  data['maps']))
