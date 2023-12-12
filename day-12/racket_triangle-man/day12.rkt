88ef792da1de784e66b687e6260eb749d1ff03bd
James Geddes <jgeddes@turing.ac.uk>
2023-12-12 20:54:54 +0000

    Part 1 -- brute force -- Racket

--- day-12/racket_triangle-man/day12.rkt
+++ day-12/racket_triangle-man/day12.rkt
@@ -2,34 +2,96 @@
 
 (module+ test
 
-  (define *eg* (string->list "#.?.?"))
+  (define *input* #<<EOF
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+EOF
+)
+
+  ;; Part 1 -- brute force (30s)
+  (for/sum ([ln (in-lines (open-input-file "input.txt"))])
+    (match-let ([(list rs gs) (parse-line ln)])
+      (count-valid-records gs (string->list rs))))
+  
+
 
 
 )
 
-(define (is-valid? xs groups skipping?)
+
+;; ------------------------------------------------------------
+
+(define (parse-line str)
+  (match-let ([(list record groups) (string-split str)])
+    (list
+     record
+     (map string->number (string-split groups ",")))))
+
+
+;; ------------------------------------------------------------
+
+;; Does this list xs satisfy the grouping?
+
+;; Let's write a little state-machine
+;; States:
+;; - "Skipping": reading "." until the next "#" or EOS (also the start state)
+;; - "In-group": in a group, reading "#" until the group count reaches 0
+;; - "at-end" : at the end of the string, should be no more groups left.
+
+;; End. Valid if there are no remaining groups
+(define (is-valid/at-end? groups)
+  (or (null? groups)            
+      (and (null? (cdr groups)) 
+           (= (car groups) 0)))) 
+
+;; Start
+(define (is-valid/skipping? groups xs)
   (if (null? xs)
-      (null? groups)
-      (let ([x (car xs)]
+      (is-valid/at-end? groups)
+      (let ([x (car xs)] 
             [r (cdr xs)])
-        (or (and (char=? x #\.)
-                 skipping?
-                 (is-valid? r groups #t))
-            (and (char=? x #\#)
-                 (not skipping?)
-                 )
-            [()])))
+        (cond
+          [(char=? x #\.) (is-valid/skipping? groups r)]
+          [else                ; next char is "#"
+           (and (pair? groups) ; some groups left!
+                (is-valid/in-group? (group-deduct-one groups) r))])))) 
 
+(define (group-deduct-one groups)
+  (cons (- (car groups) 1) (cdr groups)))
 
-
-  ;; list? -> forest?
-  (define (make-forest xs)
+;; At this point, groups is '(g . gs), where g = 0 if there should be no more #s
+(define (is-valid/in-group? groups xs)
+  (match-let ([(cons g gs) groups])
     (if (null? xs)
-        '()
-        (match (car xs)
-          [#\? (list (cons #\# (make-forest (cdr xs)))
-                     (cons #\. (make-forest (cdr xs))))]
-          [v   (list (cons v (make-forest (cdr xs))))]))))
+        (is-valid/at-end? groups)
+        (let ([x (car xs)]
+              [r (cdr xs)]
+              [g (car groups)]
+              [gs (cdr groups)])
+          (cond
+            [(char=? x #\.) (and (= g 0)
+                                 (is-valid/skipping? gs r))]
+            [else           (and (> g 0)
+                                 (is-valid/in-group? (group-deduct-one groups) r))])))))
+        
+
+(define (count-valid-records gs rs)
+  (length
+   (filter-map (curry is-valid/skipping? gs) (flatten-forest (make-forest rs)))))
+
+
+;; list? -> forest?
+(define (make-forest xs)
+  (if (null? xs)
+      '()
+      (match (car xs)
+        [#\? (list (cons #\# (make-forest (cdr xs)))
+                   (cons #\. (make-forest (cdr xs))))]
+        [v   (list (cons v (make-forest (cdr xs))))])))
 
  ;; tree : '(c . f) 
 (define (tree? t)
