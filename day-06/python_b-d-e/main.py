130c082c5beb525ea7d2afd107258a977d932eca
Benjamin Etheridge <betheridge@turing.ac.uk>
2023-12-13 12:42:26 +0000

    refactor

--- day-06/python_b-d-e/main.py
+++ day-06/python_b-d-e/main.py
@@ -1,8 +1,8 @@
-def parse_input(fname):
-    lines = [list(filter(None, line.split(" "))) for line in open(fname).read().splitlines()]
-    return  [{"time": int(lines[0][i]), "distance": int(lines[1][i])} for i in range(1, len(lines[0]))]
-
-def part_one(data):
+def part_one(fname):
+    def parse_input(fname):
+        lines = [list(filter(None, line.split(" "))) for line in open(fname).read().splitlines()]
+        return  [{"time": int(lines[0][i]), "distance": int(lines[1][i])} for i in range(1, len(lines[0]))]
+    data = parse_input(fname)
     error = 1
     for race in data:
         lowest, highest = 0, race['time']
@@ -13,7 +13,4 @@ def part_one(data):
         error *= (highest - lowest + 1)
     print(error)
 
-
-# data = parse_input("example.txt")
-data = parse_input("input.txt")
-part_one(data)
+part_one("input.txt")
